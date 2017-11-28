ded0a14258e4b5780ff799ab4f5b58c8385fb216
Kazutoshi Shinoda <kazutoshi.shinoda0516@gmail.com>
2017-11-28 15:11:56 +0900

    Update

--- vocabulary.py
+++ vocabulary.py
@@ -17,13 +17,21 @@ class Vocab():
     def word2id(self, input_file, output_file):
         f = codecs.open(input_file, 'r', 'utf-8')
         g = open(output_file, 'w')
-        
+        for line in f:
+            line = line.strip().split()
+            line = map(lambda x: str(self.dic.token2id(x)), line)
+            line = u" ".join() + u"\n"
+            g.write(line)
         f.close()
         g.close()
         
     def id2word(self, input_file, output_file):
         f = open(input_file, 'r')
         g = codecs.open(output_file, 'w', 'utf-8')
-        
+        for line in f:
+            line = line.strip().split()
+            line = map(lambda x: self.dic.id2token(int(x)), line)
+            line = u" ".join() + u"\n"
+            g.write(line)
         f.close()
         g.close()
